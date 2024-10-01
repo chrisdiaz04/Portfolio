c7e05fd75c3d7ac573356845f6dbd540680c8deb
Chris Diaz <diazchris27586@gmail.com>
2024-09-30 21:12:09 -0500

    Initial commit

--- Portfolio/src/components/Projects/Projects.jsx
+++ Portfolio/src/components/Projects/Projects.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import Image from '../../assets/Algo-visual.png'; // Adjust the relative path
+
 
 function Projects() {
     return (
-        <section>
-            <div>
+        <section className="Projects-section" >
+            <h1>Projects</h1>
+            <div className="project-card">
                 
+                <article className="project">
+                    <img className="Algo" src={Image} alt="Photo of website" />
+                </article>
             </div>
+
+            <div className="project-card">
+                <article className="project">
+
+                </article>
+            </div>
+
+
         </section>
     )
 }
